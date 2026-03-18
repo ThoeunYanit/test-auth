9e63f30bd3515c15945528c3080e0d3fad30de9f
ThoeunYanit <nith.gigo156@gmail.com>
2026-03-19 01:15:20 +0700

    initialize

--- db.js
+++ db.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  family: 4,
+  ssl: {
+    rejectUnauthorized: false  // ← add this
+  }
 });
 
 module.exports = pool;
